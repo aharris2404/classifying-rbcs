065f9d7a2afc5a41ecf74a011f60526027b79a3f
Andrew <andrew.harris2404@gmail.com>
2021-10-27 14:31:03 -0400

    made some simple changes

--- connected_component.py
+++ connected_component.py
@@ -1,21 +1,17 @@
-from PIL.Image import open as image_open
-from PIL.ImageOps import grayscale
-import numpy as np
-import PIL.Image.Image
+class ImageHandler():
+    def __init__(self, array): #array might be 2d or 3d
+        self.array = array
+        self.X, self.Y = array.shape[0], array.shape[1]
+        self.connected_components = _find_connected_components()
 
-# dave = Image("./some_path.png")
-# dave.image.show()
-# dave.data
+    def _find_connected_components(self):
+        result = {}
 
-class Image():
-    def __init__(self, path):
-        self.image = image_open(path)
-        self.data = np.array(self.image)
+    def color_at(self, x, y):
+        return self.array[x][y]
 
-    def grayscale(self):
-        self.grayscale = grayscale(self.image)
-        self.grayscale_data = np.array(self.grayscale)
-
+    def get_cardinal_neighbors(self, x, y):
+        
 
 class ConnectedComponent(Image):
     def __init__(self, seed_pixel, label):
@@ -71,3 +67,4 @@ class Pixel():
                   (self.x, self.y-1)]
         neighbors = [Pixel(coordinate[0], coordinate[1]) for coordinate in result]
         return [(i, j) for (i, j) in result if (i >= 0) and i < Pixel.X and j >= 0 and j < Pixel.Y]
+
