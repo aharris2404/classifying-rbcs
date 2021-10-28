d1509b1ff12aee1f0a7545019de1753171c91383
ANekhai <30994106+ANekhai@users.noreply.github.com>
2021-10-28 15:47:15 -0400

    Refactored Image and Component classes

--- connected_component.py
+++ connected_component.py
@@ -1,22 +1,72 @@
+import numpy as np
+
 class ImageHandler():
     def __init__(self, array): #array might be 2d or 3d
-        self.array = array
-        self.X, self.Y = array.shape[0], array.shape[1]
-        self.connected_components = _find_connected_components()
+        try:
+            if len(shape.array) > 2:
+                raise NotImplementedError
+            
+            self.array = array
+            self.X, self.Y = array.shape[0], array.shape[1]
+            self.connected_components = self._find_connected_components()
+        except Exception as inst: 
+            print("Only grayscale images supported currently")
 
-    def _find_connected_components(self):
+
+    def _grow_component(self, seed, unvisited):
+
+        queue = [seed]
+        component = {seed}
+
+        while queue:
+            curr_pixel = queue.pop()
+            all_neighbors = self._get_cardinal_neighbors(curr_pixel)
+            unvisited_neighbors = [neighbor for neighbor in all_neighbors 
+                                    if self.color_at(neighbor) == self.color_at(curr_pixel)
+                                    and neighbor in unvisited]
+
+            unvisited = unvisited - set(unvisited_neighbors)
+            component.add(unvisited_neighbors)
+
+            queue.extend(unvisited_neighbors)
+
+        return component
+
+
+    def _find_connected_components(self, color=255):
+        xs, ys = np.where(self.array == color)
+        unvisited_pixels = set(zip(xs, ys))
         result = {}
+        label = 0
 
-    def color_at(self, x, y):
+        while unvisited_pixels:
+            seed_pixel = unvisited_pixels.pop()
+
+            curr_component = self._grow_component(seed_pixel, unvisited_pixels)  
+            result.add(ConnectedComponent(seed_pixel, curr_component, label))
+            label += 1
+        
+        return result
+
+
+    def color_at(self, loc):
+        x, y = loc
         return self.array[x][y]
 
-    def get_cardinal_neighbors(self, x, y):
-        
+    def _get_cardinal_neighbors(self, loc):
+        x, y = loc
+        neighbors = [(x - 1, y), (x + 1, y), (x, y-1), (x, y+1)]
+        return [(i, j) for i, j in neighbors if 0 <= i and i < self.X and 0 <= j and j < self.Y]
 
-class ConnectedComponent(Image):
-    def __init__(self, seed_pixel, label):
+
+    def _filter_contained_boxes(self):
+        # compare boxes around connected components and merge any box contained in another box
+        return None
+
+class ConnectedComponent():
+    def __init__(self, seed_pixel, component, label): #refactor to move growing components into image handler
         self.seed_pixel = seed_pixel
-        self.pixels = {seed_pixel}
+        self.pixels = component
         self.label = label
         self.bound_box = {}
 
@@ -26,23 +76,17 @@ class ConnectedComponent(Image):
     def update_bound_box(self):
         pixels = self.pixels.copy()
         while pixels:
-            comparee = pixels.pop()
-            self.bound_box['x_min'] = min(self.bound_box.get('x_min', comparee.x), comparee.x)
-            self.bound_box['x_max'] = max(self.bound_box.get('x_max', comparee.x), comparee.x)
-            self.bound_box['y_min'] = min(self.bound_box.get('y_min', comparee.y), comparee.y)
-            self.bound_box['y_max'] = max(self.bound_box.get('y_max', comparee.y), comparee.y)
-
-    def gobble(self): #add new pixels that are nearby from a seed
-        queue = self.pixels.copy()
-
-        while queue():
-            curr_pixel = queue.pop()
-            neighbors = curr_pixel.get_cardinal_neighbors()
-
-            for neighbor in neighbors:
-                if neighbor.color == curr_pixel.color and neighbor not in self.pixels:
-                    self.pixels.add(neighbor)
-                    queue.add(neighbor)
+            x, y = pixels.pop()
+            self.bound_box['x_min'] = min(self.bound_box.get('x_min', x), x)
+            self.bound_box['x_max'] = max(self.bound_box.get('x_max', x), x)
+            self.bound_box['y_min'] = min(self.bound_box.get('y_min', y), y)
+            self.bound_box['y_max'] = max(self.bound_box.get('y_max', y), y)
+    
+    def __hash__(self):
+        return hash(self.label)
+        
+    def __add__(self, new_component):
+        return None
 
 class Pixel():
     X=None #the size of the x dimension of the image
@@ -67,4 +111,3 @@ class Pixel():
                   (self.x, self.y-1)]
         neighbors = [Pixel(coordinate[0], coordinate[1]) for coordinate in result]
         return [(i, j) for (i, j) in result if (i >= 0) and i < Pixel.X and j >= 0 and j < Pixel.Y]
-
