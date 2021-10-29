6abf8af4f11f3e919674d8a4e13810d0867b8133
ANekhai <30994106+ANekhai@users.noreply.github.com>
2021-10-29 12:32:58 -0400

    Fixed ImageHandler bugs and added tests

--- src/image_handler/image_handler.py
+++ src/image_handler/image_handler.py
@@ -1,16 +1,15 @@
 import numpy as np
-from connected_component import ConnectedComponent
+from src.image_handler.connected_component import ConnectedComponent
 
 class ImageHandler():
     def __init__(self, array): #array might be 2d or 3d
         try:
-            if len(self.array.shape) > 2:
+            if len(array.shape) > 2:
                 raise NotImplementedError
             
             self.array = array
-            self.X, self.Y = array.shape[0], array.shape[1]
-            self.connected_components = self._find_connected_components()
-            self._filter_contained_boxes()
+            self.X, self.Y = array.shape[0], array.shape[1]            
+            # self.connected_components = self._find_connected_components()
         except Exception as inst: 
             print("Only grayscale images supported currently")
 
@@ -28,7 +27,7 @@ class ImageHandler():
                                     and neighbor in unvisited]
 
             unvisited = unvisited - set(unvisited_neighbors)
-            component.add(unvisited_neighbors)
+            component |= set(unvisited_neighbors)
 
             queue.extend(unvisited_neighbors)
 
@@ -38,13 +37,14 @@ class ImageHandler():
     def _find_connected_components(self, color=255):
         xs, ys = np.where(self.array == color)
         unvisited_pixels = set(zip(xs, ys))
-        result = {}
+        result = set()
         label = 0
 
         while unvisited_pixels:
             seed_pixel = unvisited_pixels.pop()
 
-            curr_component = self._grow_component(seed_pixel, unvisited_pixels)  
+            curr_component = self._grow_component(seed_pixel, unvisited_pixels)
+            unvisited_pixels -= curr_component
             result.add(ConnectedComponent(seed_pixel, curr_component, label))
             label += 1
         
@@ -61,7 +61,7 @@ class ImageHandler():
         return [(i, j) for i, j in neighbors if 0 <= i and i < self.X and 0 <= j and j < self.Y]
 
 
-    def _filter_contained_boxes(self):
+    def filter_contained_boxes(self):
         # compare boxes around connected components and merge any box contained in another box
         # initial pass is O(n^2), can be improved to O(nlogn)
 
--- tests/image_handler/test_image_handler.py
+++ tests/image_handler/test_image_handler.py
@@ -1,27 +1,46 @@
 import unittest
+import pathlib
+
+from PIL import Image, ImageOps
+import numpy as np
+
+from src.image_handler.image_handler import ImageHandler
+
+curr_path = pathlib.Path(__file__).parent.resolve()
 
 class TestImageHandler(unittest.TestCase):
 
+
     def setUp(self):
-        self.image_path = ""
-
-    def tearDown(self):
-        pass
-
+        two_test_path = curr_path.joinpath("two_test.png")
+        im = Image.open(two_test_path)
+        im_gray = ImageOps.grayscale(im)
+        im_array = np.array(im)
+        gray_array = np.array(im_gray)
+        self.image = ImageHandler(gray_array)
+        
 
     def test_color_at(self):
-        pass
-
-    def test_cardinal_neighbors(self):
-        pass
+        self.assertEqual(self.image.color_at((0, 0)), 0, "Top left pixel of two_test.png is black")
+        self.assertEqual(self.image.color_at((3, 3)), 255, "Central pixel of two_test.png is white")
 
     def test_grow_component(self):
-        pass
+        component = self.image._grow_component((1,1), set([(i, j) for i in range(self.image.X) 
+                                                                  for j in range(self.image.Y)]))
+        self.assertEqual(len(component), 16, "Outer white ring should contain no white pixels")
+        self.assertNotIn((0,0), component, "Outer black ring should not be in component")
+        self.assertNotIn((2,2), component, "Inner black ring should not be in component")
+        self.assertNotIn((3,3), component, "Inner white ring should not be in component")
 
     def test_find_connected_components(self):
-        pass
+        components = self.image._find_connected_components(color=255)
+        self.assertEqual(len(components), 2, "Two white components in two_test.png")
+        self.assertListEqual(sorted([len(c.pixels) for c in components]), [1, 16], "Sorted lengths of two white components in two_test.png")
 
     def test_filter_contained_boxes(self):
+        pass
+
+    def test_color_image_failure(self):
         pass
 
 
