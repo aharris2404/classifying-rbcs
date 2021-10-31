051fe2661d08b273759e0fc716eb74f6017c2602
ANekhai <30994106+ANekhai@users.noreply.github.com>
2021-10-31 18:47:50 -0400

    Added to main function

--- src/main.py
+++ src/main.py
@@ -1,8 +1,21 @@
-from image_handler import ImageHandler, Filters
+from pathlib import Path
 
+import numpy as np
+
+from image_handler.image_handler import ImageHandler
+from image_handler.filters import Filters
 
 def main():
-    pass
+    filters = Filters()
+    im_folder = Path("images")
+
+    p = im_folder.glob("**/*")
+    images = [x for x in p if x.is_file()]
+
+    for image in images:
+        im_array = filters.run_initial_pipeline(image)
+        im_graph = ImageHandler(im_array)
+
 
 
 if __name__ == "__main__":
